4f173b9156385cc77664132144f905c18009a02c
alexey.zavizionov@gmail.com <alexey.zavizionov@gmail.com@c473c225-86bf-c8f8-415c-1c9fe8c00e1f>
2011-11-01 07:16:31 +0000

    Fixed css fo login.jsp

--- trunk/vk-java-login-oauth-servlet/src/main/java/org/zav/server/VkFilter.java
+++ trunk/vk-java-login-oauth-servlet/src/main/java/org/zav/server/VkFilter.java
@@ -9,7 +9,6 @@ import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
-import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 
 import org.slf4j.Logger;
